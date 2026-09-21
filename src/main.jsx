d7e059cb39676145cef9063a21560b546f9e2279
subinsudkuo-source <subinsudkuo@gmail.com>
2026-09-21 16:04:46 +0700

    Add src/main.jsx

---
+++ src/main.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from '../upload/ar_flow_adventure.tsx';
+import './styles.css';
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+);
